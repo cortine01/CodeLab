c1301d2f7e0b525226fdec7bf9ed15752b30ae02
anny <annyjaimes10@gmail.com>
2024-05-24 00:32:12 -0500

    se cambiaron los colores y las imagenes del parallax

--- function.js
+++ function.js
@@ -1,18 +1,23 @@
 let text = document.getElementById('text');
-let leaf = document.getElementById('leaf');
-let hill1 = document.getElementById('hill1');
-let hill4 = document.getElementById('hill4');
-let hill5 = document.getElementById('hill5');
+let man = document.getElementById('man');
+let woman = document.getElementById('woman');
+let city = document.getElementById('city');
+let city2 = document.getElementById('city2');
+let world = document.getElementById('world');
 
 window.addEventListener('scroll', () => {
    let value = window.scrollY;
-   
+   //if (value == 0){
+    //value = 1;
+    //man.style.left += 800 + 'px' ;
+  // };
    text.style.marginTop = value * 2.5 + 'px';
-   leaf.style.top = value * -1.5 + 'px';
-   leaf.style.left = value * 1.5 + 'px';
-   hill5.style.left = value * 1.5 + 'px';
-   hill4.style.left = value * -1.5 + 'px';
-   hill1.style.top = value * 1 + 'px';
+   //man.style.left = value * 0.1 + 'px';
+   man.style.top = value * -0.1 + 'px';
+   city2.style.top = value * 1 + 'px';
+   city.style.top = value * 1 + 'px';
+   woman.style.top = value * -0.1 + 'px';
+   world.style.top = value * 1.5 + 'px';
 });
 
 
